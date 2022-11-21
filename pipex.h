e6356d925bc388cc1a3986b776571e854d281b87
stena-he <stena-he@student.42wolfsburg.de>
2022-11-21 22:43:41 +0100

    Add basic libraries and structure

---
+++ pipex.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 20:51:45 by stena-he          #+#    #+#             */
+/*   Updated: 2022/11/19 21:54:02 by stena-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+/* Libraries */
+//Std libraries
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+//Custom libraries
+# include "libraries/ft_printf/libft/libft.h"
+# include "libraries/ft_printf/ft_printf.h"
+
+/* Structs */
+//Stacks 
+// typedef struct s_stack
+// {
+// 	int				value;
+// 	int				index;
+// 	int				pos;
+// 	int				target_pos;
+// 	int				cost_a;
+// 	int				cost_b;
+// 	struct s_stack	*next;
+// }					t_stack;
+
+/* Functions */
+
+// Title //
+
+
+// Title //
+//Subtitle
+
+
+#endif

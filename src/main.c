0094bfd76dd68602c3d067aae6dee7e0bcb7d09e
stena-he <stena-he@student.42wolfsburg.de>
2022-11-23 23:34:44 +0100

    Refactor execution func into its script

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 21:51:11 by stena-he          #+#    #+#             */
-/*   Updated: 2022/11/21 23:16:52 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/11/23 23:33:06 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,44 @@
 
 int main(int argc, char **argv, char **envp) 
 {	
-	char	*cmd_path;
-	char	*options[3] = {argv[1], argv[2], NULL};
-	char	*cmd = argv[1];
+	int	fd[2];
+	int	pid1;
+	int	pid2;
 	
 	if (argc < 2)
 		return (1); // Not enough arguments
-	cmd_path = get_cmd_path(cmd, envp);
-	if (!cmd_path)
+	
+	if (pipe(fd) == -1)
+		return (2); // Pipe error
+	
+	pid1 = fork();
+	if (pid1 < 0)
+		return (3); // fork error
+
+	if (pid1 == 0)
 	{
-		perror(cmd_path);
-		return(-1);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		do_cmd(argv, envp);
 	}
-	execve(cmd_path, options, envp);
-	free(cmd_path);
+
+	pid2 = fork();
+	if (pid2 < 0)
+		return (3); // fork error
+	
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		do_cmd(argv, envp);
+	}
+	close(fd[0]);
+	close(fd[1]);
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	
 	return (0);
 }

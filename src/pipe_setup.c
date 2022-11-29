2e0877360f4c0141ea296c60c1fc365a564a0fd9
stena-he <stena-he@student.42wolfsburg.de>
2022-11-30 00:31:59 +0100

    Add fd swaping in mandatory

--- src/pipe_setup.c
+++ src/pipe_setup.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 22:24:51 by stena-he          #+#    #+#             */
-/*   Updated: 2022/11/30 00:11:12 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/11/30 00:30:10 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	pipe_setup(int argc, char **argv, char **envp)
 {
-	int	fd[2];
+	int prev[2];
+	int	next[2];
 	int	pid;
 	int	file_in;
 	int file_out;
@@ -22,11 +23,10 @@ int	pipe_setup(int argc, char **argv, char **envp)
 	
 	file_in = 0;
 	file_out = 0;
-	if (pipe(fd) == -1)
-		return (2); // pipe error
-		
-	// Start
 	i = 1;
+	// Start
+	if (pipe(next) == -1)
+		return (2); // pipe error
 	pid = fork();
 	if (pid < 0)
 		return (3); // fork error
@@ -34,9 +34,9 @@ int	pipe_setup(int argc, char **argv, char **envp)
 	{
 		file_in = open(argv[i], O_RDONLY, 0666);
 		dup2(file_in, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
+		dup2(next[1], STDOUT_FILENO);
+		close(next[1]);
+		close(next[0]);
 		close(file_in);
 		do_cmd(argv[i + 1], envp);
 	}
@@ -46,44 +46,49 @@ int	pipe_setup(int argc, char **argv, char **envp)
 	// Middle
 	if (argc > 5)
 	{
-		while (i < (argc - 2))
-		{
-			if (pipe(fd) == -1)
-				return (2); // pipe error
-			pid = fork();
-			if (pid < 0)
-				return (3); // fork error
-			if (pid == 0)
-			{
-				dup2(fd[0], STDIN_FILENO);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				do_cmd(argv[i], envp);
-			}
-			waitpid(pid, NULL, 0);
-			i++;
-		}
+		// while (i < (argc - 2))
+		// {
+		// 	if (pipe(fd) == -1)
+		// 		return (2); // pipe error
+		// 	pid = fork();
+		// 	if (pid < 0)
+		// 		return (3); // fork error
+		// 	if (pid == 0)
+		// 	{
+		// 		dup2(fd[0], STDIN_FILENO);
+		// 		dup2(fd[1], STDOUT_FILENO);
+		// 		close(fd[0]);
+		// 		close(fd[1]);
+		// 		do_cmd(argv[i], envp);
+		// 	}
+		// 	waitpid(pid, NULL, 0);
+		// 	i++;
+		// }
+		return (0);
 	}
 
 	// Last
+	prev[0] = next[0];
+	prev[1] = next[1];
 	pid = fork();
 	if (pid < 0)
 		return (3); // fork error
 	if (pid == 0)
 	{
 		file_out = open(argv[i + 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
-		dup2(fd[0], STDIN_FILENO);
+		dup2(prev[0], STDIN_FILENO);
 		dup2(file_out, STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
+		close(prev[0]);
+		close(prev[1]);
+		close(next[0]);
+		close(next[1]);
 		close(file_out);
 		do_cmd(argv[i], envp);
 	}
-	close(fd[0]);
-	close(fd[1]);
-	close(file_in);
-	close(file_out);
+	close(prev[0]);
+	close(prev[1]);
+	close(next[0]);
+	close(next[1]);
 	
 	waitpid(pid, NULL, 0);
 	return (0);

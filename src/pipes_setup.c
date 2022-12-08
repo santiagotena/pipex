45c978b1e0c4e1079da656cf8407b87b8c5c9a4c
stena-he <stena-he@student.42wolfsburg.de>
2022-12-08 23:57:02 +0100

    Add a stable version

--- src/pipes_setup.c
+++ src/pipes_setup.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 22:24:51 by stena-he          #+#    #+#             */
-/*   Updated: 2022/12/08 20:47:00 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/12/08 23:51:48 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,12 @@ t_data	*assign_inputs(int argc, char **argv, char **envp)
 	return (node);	
 }
 
-void	first_pipe(t_data **inputs, int fd[MAX_FD][2])
+void	first_pipe(t_data **inputs)
 {
+	printf("Enter First pipe \n");
 	int	pid;
 	int	file_in;
+	int fd[MAX_FD][2];
 	
 	file_in = 0;
 	if (pipe(fd[(*inputs)->j]) < 0)
@@ -51,68 +53,86 @@ void	first_pipe(t_data **inputs, int fd[MAX_FD][2])
 		do_cmd((*inputs)->argv[(*inputs)->i + 1], (*inputs)->envp);
 	}
 	waitpid(pid, NULL, 0);
+	printf("Exit First pipe \n");
+}
+
+void	mid_pipes(t_data **inputs)
+{
+	printf("Enter Middle pipe \n");
+	int	pid;
+	int fd[MAX_FD][2];
+	
+	// Middle
+	while ((*inputs)->i < ((*inputs)->argc - 2))
+	{
+		if (pipe(fd[(*inputs)->j]) < 0)
+			return ; // pipe error
+		pid = fork();
+		if (pid < 0)
+			return ; // fork error
+		if (pid == 0)
+		{
+			dup2(fd[(*inputs)->j - 1][0], STDIN_FILENO);
+			dup2(fd[(*inputs)->j][1], STDOUT_FILENO);
+			
+			close_fds((*inputs)->argc, fd);
+			
+			do_cmd((*inputs)->argv[(*inputs)->i], (*inputs)->envp);
+		}
+		// waitpid(pid, NULL, 0);
+		(*inputs)->i++;
+		(*inputs)->j++;
+	}
+	printf("Exit Middle pipe \n");
+}
+
+void	last_pipe(t_data **inputs)
+{
+	printf("Enter Last pipe \n");
+	int	pid;
+	int	file_out;
+	int fd[MAX_FD][2];
+	
+	file_out = 0;
+	pid = fork();
+	if (pid < 0)
+		return ; // fork error
+	if (pid == 0)
+	{
+		// printf("Checkpoint 2 Last pipe \n");
+		file_out = open((*inputs)->argv[(*inputs)->i + 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
+		// printf("Checkpoint 2.5 Last pipe \n");
+		dup2(fd[(*inputs)->j - 1][0], STDIN_FILENO);
+		// printf("Checkpoint 3 Last pipe \n");
+		dup2(file_out, STDOUT_FILENO);
+		// fprintf(stderr, "Checkpoint 4 Last pipe \n");
+		close(file_out);
+		// fprintf(stderr, "Checkpoint 5 Last pipe \n");
+		close_fds((*inputs)->argc, fd);
+		// fprintf(stderr, "Checkpoint 6 Last pipe \n");
+
+	
+		do_cmd((*inputs)->argv[(*inputs)->i], (*inputs)->envp);
+		fprintf(stderr, "Checkpoint 7 Last pipe \n");
+	}
+	waitpid(pid, NULL, WNOHANG);
+	printf("Exit Last pipe \n");
 }
 
 int	pipes_setup(int argc, char **argv, char **envp)
 {
-	int fd[MAX_FD][2]; //keep
-	int	pid; //remove
-	int file_out; //remove
-	int	i; //keep
-	int j; //keep
 	t_data	*inputs;
-	
-	file_out = 0; //remove
-	i = 1; //keep
-	j = 0; //keep
-	
-	inputs = assign_inputs(argc, argv, envp);
-	first_pipe(&inputs, fd);
-	i = i + 2;
-	j++;
-	// Middle
-	if (argc > 5)
-	{
-		while (i < (argc - 2))
-		{
-			if (pipe(fd[j]) < 0)
-				return (2); // pipe error
-			pid = fork();
-			if (pid < 0)
-				return (3); // fork error
-			if (pid == 0)
-			{
-				dup2(fd[j - 1][0], STDIN_FILENO);
-				dup2(fd[j][1], STDOUT_FILENO);
-				
-				close_fds(argc, fd);
-				
-				do_cmd(argv[i], envp);
-			}
-			// waitpid(pid, NULL, 0);
-			i++;
-			j++;
-		}
-	}
-
-	// Last
-	pid = fork();
-	if (pid < 0)
-		return (3); // fork error
-	if (pid == 0)
-	{
-		file_out = open(argv[i + 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
-		dup2(fd[j - 1][0], STDIN_FILENO);
-		dup2(file_out, STDOUT_FILENO);
-		close(file_out);
 		
-		close_fds(argc, fd);
+	inputs = assign_inputs(argc, argv, envp);
+	first_pipe(&inputs);
 	
-		do_cmd(argv[i], envp);
-	}
+	inputs->i = inputs->i + 2;
+	inputs->j++;
 	
-	close_fds(argc, fd);
-	
-	waitpid(pid, NULL, 0);
+	if (inputs->argc > 5)
+		mid_pipes(&inputs);
+
+	last_pipe(&inputs);
+	// close_fds(inputs->argc, fd);
 	return (0);
 }
